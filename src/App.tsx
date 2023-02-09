6c460f808dfb77c39803e48de91752364decf22b
Nikita Kulakov <nikita.kulakov@raiffeisen.ru>
2023-02-09 22:11:44 +0300

    apply formatters

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p onClick={() => console.log("asd")}>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
